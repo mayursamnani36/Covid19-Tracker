17bd32526a49fb5024ff6fd54df80fb5417449b3
mayursamnani36 <54547503+mayursamnani36@users.noreply.github.com>
2020-11-26 20:52:06 +0530

    Updated Country picker component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 import styles from './App.module.css';
-import {fetchData} from './api';
-
+import { fetchData } from './api';
+import coronaImage from './images/image.png';
 class App extends React.Component {
 
   state = {
     data: {},
+    country: '',
   }
 
   async componentDidMount() {
@@ -17,13 +18,20 @@ class App extends React.Component {
     this.setState({ data: fetchedData });
   }
 
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    // console.log(country);
+    this.setState({ data: fetchedData, country: country });
+  }
+
   render() {
-    const data = this.state.data;
+    const {data, country} = this.state;
     return (
       <div className={styles.container}>
+        <img src={coronaImage} className={styles.image} alt='COVID-19'/>
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
